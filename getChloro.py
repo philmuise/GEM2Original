fa761a1966969bbec1ff4cba623503fe000881b4
philmuise <33460479+philmuise@users.noreply.github.com>
2017-11-23 16:27:51 -0500

    Add files via upload

--- getChloro.py
+++ getChloro.py
@@ -5,7 +5,8 @@
 # HISTORY                                                                      #
 # -------                                                                      #
 # Developed by D. Hennessy, January 2017.                                      #
-# Last modified: 31 March 2017 by D. Hennessy.                                 #
+# Modified by Philippe Muise, September 2017                                   #
+# Last modified: 15 Sept 2017 by Philippe Muise.                               #
 #==============================================================================#
 """USAGE
 Module imported and used as the "1a_Download Chlorophyll_a NetCDF Files" script
@@ -13,7 +14,8 @@ tool in the "GEM2_Oil_Seep_Detection_Analysis" Python Toolbox.
 
 SUMMARY
 Downloads MODIS chlorophyll_a data for those acquisition days where a corresponding
-acquisition day for a dark targets feature class exists.
+acquisition day for a dark targets feature class exists. If specified, chlorophyll_a data
+is acquired for a range of days around each dark targets feature class.
 
 INPUT
 - Folder Location of Dark Targets GDBs (user input): Folder containing the geodatabases
@@ -23,6 +25,10 @@ chlorophyll_a raster data.
 
 - Chlorophyll_a Data FTP Directory (default user input): String parameter containing
 the FTP directory where the chlorophyll_a MODIS data is available for download.
+
+- Chlorophyll Day Range Size From Date of Acquisition (user input): Integer parameter
+indicating the range of days +/- from the date of acquistion of the dark features
+feature class.
 
 OUTPUT
 - Chlorophyll_a NetCDF Files (automated output): Chlorophyll_a raster data in
@@ -41,14 +47,16 @@ ADDITIONAL FUNCTIONS (explained in script below)
 import arcpy
 import os
 import datetime
+import ftplib
 from ftplib import FTP
 import logging
+import time
 
 
 class getChloro(object):
     def __init__(self):
         """Define the tool (tool name is the name of the class)."""
-        self.label = "1a_Download Chlorophyll_a NetCDF Files"
+        self.label = "3a. Download Chlorophyll_a NetCDF Files"
         self.description = "Downloads MODIS chlorophyll_a data for each\
          RADARSAT-2 acquisition found in the dark targets yearly GDBs."
         self.canRunInBackground = False
@@ -68,10 +76,18 @@ class getChloro(object):
             datatype="GPString",
             parameterType="Required",
             direction="Input")
-
         params1.value = 'podaac-ftp.jpl.nasa.gov/allData/modis/L3/aqua/chlA/v2014.0/4km/daily/'
 
-        params = [params0, params1]
+        params2 = arcpy.Parameter(
+            displayName="Chlorophyll Day Range",
+            name="Day Range",
+            datatype="GPLong",
+            parameterType="Required",
+            direction="Input")
+
+
+
+        params = [params0, params1, params2]
 
         return params
 
@@ -107,6 +123,7 @@ class getChloro(object):
         if not os.path.exists(local_chloroFolder):
             os.makedirs(local_chloroFolder)
         ftpDir = parameters[1].valueAsText
+        dayRange = parameters[2].value
 
         # Set value for FTP address of host site for MODIS data
         fileHostPath = ftpDir.split('/')[0]
@@ -120,7 +137,7 @@ class getChloro(object):
         arcpy.AddMessage("Workspace contains the following " + str(len(gdbList)) + " GDBs: " + str(gdbList))
 
         # Connect to ftp host
-        arcpy.AddMessage("\nConnecting to host ftp site: " + fileHostPath)
+        arcpy.AddMessage("\nConnecting to host ftp site2: " + fileHostPath)
         ftp = FTP(fileHostPath)
         ftp.login()
         logging.info("Login established to FTP host '%s'\n", fileHostPath)
@@ -133,19 +150,32 @@ class getChloro(object):
             arcpy.env.workspace = gdb
             logging.info("Processing '%s' geodatabase", gdb)
             fcList = arcpy.ListFeatureClasses()
+
             arcpy.AddMessage("\nGDB contains the following " + str(len(fcList)) + " feature classes: " + str(fcList))
             logging.info("List of acquisition days requiring file downloads: '%s'\n", str(fcList))
 
             # Iterate through feature classes in GDB
             for fc in fcList:
-
+                arcpy.AddMessage("Started to go through days")
                 # Determine corresponding year and date path for location on ftp server
-                dateFolder = self.yearDay(fc.split('_')[1])
-                chloroFilePath = fileDir + dateFolder
+                dateFolder = self.yearDay(fc.split('_')[1], dayRange)
+                arcpy.AddMessage("{}".format(dateFolder))
 
-                # Initiate download of files for current feature class
-                logging.info("Attempting file downloads for '%s' feature class\n", fc)
-                self.getChloroFile(ftp, chloroFilePath, local_chloroFolder)
+                for day in dateFolder:
+                    chloroFilePath = fileDir + day
+                    arcpy.AddMessage("{}".format(chloroFilePath))
+                    # Initiate download of files for current feature class
+                    logging.info("Attempting file downloads for '%s' feature class\n", fc)
+                    for tries in range(5):
+                      try:
+                             self.getChloroFile(ftp, chloroFilePath, local_chloroFolder)
+                             break
+                      except ftplib.all_errors as e:
+                             x = 20
+                             arcpy.AddMessage('\n --------------------ERROR: FAILED ATTEMPT #{a} of 5. WILL ATTEMPT TO DOWNLOAD FILE AGAIN IN {x} SECONDS-----------------\n'.format(a = tries,x = x))
+                             time.sleep(x)
+
+
 
             logging.info("Processing for '%s' geodatabase complete\n", gdb)
 
@@ -156,22 +186,35 @@ class getChloro(object):
 
         return
 
-    def yearDay(self, fc_dateString):
-        """Calculate the day of the year for each RADARSAT-2 acquisition (to conform to MODIS naming convention)
+    def yearDay(self, fc_dateString, dayRange):
+        """Calculate the day of the year for each RADARSAT-2 acquisition (to conform to MODIS naming convention) and the day of year within the range desired
 
         Parameter:
             fc_dateString = Date string acquired from the feature class name in the input geodatabase,
             conforming to the following format: YYYYmmDD (e.g. 20100925 for 25 September 2010)
+            dayRange = Integer input by user that sets the range +/- of days around fc_dateString for download of chlorophyll data.
 
         Return:
-            Returns sub-directory string to be concatenated with ftp file directory in order to point to the required year and day of the year to access the desired MODIS imagery,
-            conforming to the following format: YYYY/DDD (e.g. 2010/268 for 25 September 2010)"""
+            Returns list of sub-directory strings to be concatenated with ftp file directory in order to point to the required year and day of the year to access the desired MODIS imagery,
+            conforming to the following format: YYYY/DDD (e.g. 2010/268 for 25 September 2010)
+        Edits:
+              Originally developed by David H. Altered by Philippe Muise
+        """
         year = fc_dateString[:4]
         month = fc_dateString[4:6].lstrip("0")
         day = fc_dateString[-2:].lstrip("0")
         fc_date = datetime.date(int(year), int(month), int(day))
-        yDay = fc_date.timetuple().tm_yday
-        return year + '/' + str(yDay).rjust(3, '0')
+        fc_dateList = [fc_date]
+        for i in range(dayRange):
+            fc_dateList.insert(0,fc_date - datetime.timedelta(days = i + 1))
+            fc_dateList.append(fc_date + datetime.timedelta(days = i + 1))
+
+        chloroDateList = []
+        for chloroDate in fc_dateList:
+            yDay = chloroDate.timetuple().tm_yday
+            chloroDateList.append(str(chloroDate.year) + '/' + str(yDay).rjust(3,'0'))
+
+        return chloroDateList
 
     def getChloroFile(self, ftp, chloroFilePath, localChloro):
         """Download chlorophyll_a data corresponding to the identified RADARSAT-2 acquisition day.
@@ -186,6 +229,8 @@ class getChloro(object):
         # Change working directory on ftp server to required year and day of the chlorophyll_a data.
         logging.info("Accessing '%s' on ftp server", chloroFilePath)
         ftp.cwd(chloroFilePath)
+        arcpy.AddMessage("{}".format(chloroFilePath))
+
 
         # List all files in current ftp server folder (should be 1x .nc file and 1x .md5 file)
         files = ftp.nlst()
